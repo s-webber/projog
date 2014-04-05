fc227582fa0e1e7b2a2799ced611bc98fd355a57
webber-s <webbers.github@gmail.com>
2014-04-05 18:54:49 +0100

    added more number\1 tests

--- src/core/org/projog/core/function/classify/IsNumber.java
+++ src/core/org/projog/core/function/classify/IsNumber.java
@@ -21,9 +21,19 @@ import org.projog.core.term.TermType;
 
 /* SYSTEM TEST
  % %TRUE% number(1)
+ % %TRUE% number(-1)
+ % %TRUE% number(0)
+ % %TRUE% number(0.0)
+ % %TRUE% number(1.0)
+ % %TRUE% number(-1.0)
+ % %FALSE% number('1')
+ % %FALSE% number('1.0')
+ % %FALSE% number(1+1)
  % %FALSE% number(a)
  % %FALSE% number(p(1,2,3))
- % %FALSE% number([a,b,c])
+ % %FALSE% number([1,2,3])
+ % %FALSE% number([a,b,c]) 
+ % %FALSE% number([])
  % %FALSE% number(X)
  */
 /**

c962c16483e2edd1ec59a19bd5beb8b1229346ef
webber-s <webbers.github@gmail.com>
2014-05-24 16:45:53 +0100

    changed return type

--- src/core/org/projog/core/function/math/IntegerDivide.java
+++ src/core/org/projog/core/function/math/IntegerDivide.java
@@ -5,14 +5,13 @@ import static org.projog.core.term.TermUtils.toInt;
 import org.projog.core.Calculatable;
 import org.projog.core.KnowledgeBase;
 import org.projog.core.term.IntegerNumber;
-import org.projog.core.term.Numeric;
 import org.projog.core.term.Term;
 
 public class IntegerDivide implements Calculatable {
    @Override
-   public Numeric calculate(KnowledgeBase kb, Term[] args) {
-      int dividend = toInt(args[0]);
-      int divisor = toInt(args[1]);
+   public IntegerNumber calculate(KnowledgeBase kb, Term[] args) {
+      final int dividend = toInt(args[0]);
+      final int divisor = toInt(args[1]);
       return new IntegerNumber(dividend / divisor);
    }
 }

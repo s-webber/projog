7f09dcdde4a20b8d3c68468e1659b9edfd095b0a
webber-s <webbers.github@gmail.com>
2014-06-28 14:34:45 +0100

    added bitwise "and" operation - resolves #52

--- etc/projog-bootstrap.pl
+++ etc/projog-bootstrap.pl
@@ -127,6 +127,7 @@
 ?- pj_add_calculatable(mod/2, 'org.projog.core.function.math.Modulo').
 ?- pj_add_calculatable(rem/2, 'org.projog.core.function.math.Remainder').
 ?- pj_add_calculatable(random/1, 'org.projog.core.function.math.Random').
+?- pj_add_calculatable('/\'/2, 'org.projog.core.function.math.BitwiseAnd').
 
 % definite clause grammers (DCG)
 ?- op(1200, xfx, '-->').
@@ -161,4 +162,5 @@
 ?- op(400, yfx, '//').
 ?- op(400, yfx, 'mod').
 ?- op(400, yfx, 'rem').
+?- op(400, yfx, '/\').
 ?- op(200, fy, '-').

02a42609da26dff0a33c3eac7e99134054d38ec0
webber-s <webbers.github@gmail.com>
2014-07-05 19:31:30 +0100

    convert_time/3 - specify timezone #57

--- src/core/org/projog/core/function/time/ConvertTime.java
+++ src/core/org/projog/core/function/time/ConvertTime.java
@@ -2,7 +2,7 @@ package org.projog.core.function.time;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
+import java.util.TimeZone;
 
 import org.projog.core.function.AbstractSingletonPredicate;
 import org.projog.core.term.Atom;
@@ -10,16 +10,15 @@ import org.projog.core.term.Term;
 
 /* TEST
  %QUERY convert_time(0, X)
- %ANSWER X=1970-01-01T01:00:00.000+0100
- %TRUE convert_time(0, '1970-01-01T01:00:00.000+0100')
+ %ANSWER X=1970-01-01T00:00:00.000+0000
+ %TRUE convert_time(0, '1970-01-01T00:00:00.000+0000')
  
  %QUERY convert_time(1000*60*60*24*500+(1000*60*72), X)
- %ANSWER X=1971-05-16T02:12:00.000+0100
- %TRUE convert_time(1000*60*60*24*500+(1000*60*72), '1971-05-16T02:12:00.000+0100')
+ %ANSWER X=1971-05-16T01:12:00.000+0000
+ %TRUE convert_time(1000*60*60*24*500+(1000*60*72), '1971-05-16T01:12:00.000+0000')
  
  %QUERY convert_time(9223372036854775807, X)
  %ANSWER X=292278994-08-17T07:12:55.807+0000
- %TRUE convert_time(9223372036854775807, '292278994-08-17T07:12:55.807+0000')
  */
 /**
  * <code>convert_time(X,Y)</code> - converts a timestamp to a textual representation.
@@ -37,7 +36,9 @@ public final class ConvertTime extends AbstractSingletonPredicate {
    }
 
    private Atom createAtom(Date d) {
-      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
+      // TODO have overloaded versions of convert_time that allow the date format and timezone to be specified?
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+      sdf.setTimeZone(TimeZone.getTimeZone("GMT-0"));
       return new Atom(sdf.format(d));
    }
 }

e1e303f849267504d1588791f08eba809a4480cb
webber-s <webbers.github@gmail.com>
2014-07-06 11:08:38 +0100

    get_char/1 end-of-file handling - resolves #67

--- src/core/org/projog/core/function/io/GetChar.java
+++ src/core/org/projog/core/function/io/GetChar.java
@@ -5,25 +5,64 @@ import org.projog.core.term.Atom;
 import org.projog.core.term.Term;
 
 /* TEST
+ write_to_file(X) :-
+    open('get_char_test.tmp', write, Z),
+    set_output(Z),
+    write(X),
+    close(Z),
+    set_output('user_output').
+
+ read_from_file :-
+    open('get_char_test.tmp', read, Z),
+    set_input(Z),
+    print_contents,
+    close(Z).
+
+ print_contents :-
+    repeat,
+    get_char(C),
+    write(C),
+    nl,
+    C=='end_of_file',
+    !.
+
+ %TRUE write_to_file('abc')
+ %QUERY read_from_file
+ %OUTPUT 
+ % a
+ % b
+ % c
+ % end_of_file
+ %
+ %OUTPUT
+ %ANSWER/
+ %NO
+
  %LINK prolog-io
  */
 /**
- * <code>get_char(X)</code> - reads a character.
- * <p>
  * <code>get_char(X)</code> - reads the next character from the input stream.
+ * <p>
+ * The goal succeeds if <code>X</code> can be unified with next character read from the current input stream. Succeeds
+ * only once and the operation of moving to the next character is not undone on backtracking.
  * </p>
  * <p>
- * The goal succeeds if <code>X</code> can be matched with next character read from the current input stream. Succeeds
- * only once and the operation of moving to the next character is not undone on backtracking.
+ * If there are no more characters to read from the current input stream (i.e. if the end of the stream has been reached) 
+ * then an attempt is made to unify <code>X</code> with an atom with the value <code>end_of_file</code>.
  * </p>
  */
 public final class GetChar extends AbstractSingletonPredicate {
    @Override
    public boolean evaluate(Term argument) {
       try {
-         char c = (char) getKnowledgeBase().getFileHandles().getCurrentInputStream().read();
-         Atom a = new Atom(Character.toString(c));
-         return argument.unify(a);
+         int c = getKnowledgeBase().getFileHandles().getCurrentInputStream().read();
+         Atom next;
+         if (c == -1) {
+            next = new Atom("end_of_file");
+         } else {
+            next = new Atom(Character.toString((char) c));
+         }
+         return argument.unify(next);
       } catch (Exception e) {
          e.printStackTrace();
          return false;

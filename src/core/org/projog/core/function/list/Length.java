3973ff974ff302fe7f9885082f22a505a84761b6
webber-s <webbers.github@gmail.com>
2014-08-24 19:42:33 +0100

    modified length/2 to work when the first argument is a variable - resolves #107

--- src/core/org/projog/core/function/list/Length.java
+++ src/core/org/projog/core/function/list/Length.java
@@ -1,10 +1,15 @@
 package org.projog.core.function.list;
 
+import static org.projog.core.term.ListFactory.createList;
 import static org.projog.core.term.ListUtils.toJavaUtilList;
+
+import java.util.ArrayList;
 
 import org.projog.core.function.AbstractSingletonPredicate;
 import org.projog.core.term.IntegerNumber;
 import org.projog.core.term.Term;
+import org.projog.core.term.TermUtils;
+import org.projog.core.term.Variable;
 
 /* TEST
  %QUERY length([],X)
@@ -19,8 +24,16 @@ import org.projog.core.term.Term;
  %FALSE length([a,b|c],X)
  %FALSE length([a,b],1)
  %FALSE length([a,b],3)
- %FALSE length(X,3)
  %FALSE length(abc,3)
+ 
+ %QUERY length(X,0)
+ %ANSWER X=[]
+ 
+ %QUERY length(X,1)
+ %ANSWER X=[E0]
+ 
+ %QUERY length(X,3)
+ %ANSWER X=[E0,E1,E2]
  */
 /**
  * <code>length(X,Y)</code> - determines the length of a list.
@@ -32,6 +45,23 @@ import org.projog.core.term.Term;
 public final class Length extends AbstractSingletonPredicate {
    @Override
    public boolean evaluate(final Term list, final Term expectedLength) {
+      if (list.getType().isVariable()) {
+         final int length = TermUtils.toInt(expectedLength);
+         return list.unify(createListOfLength(length));
+      } else {
+         return checkLength(list, expectedLength);
+      }
+   }
+
+   private Term createListOfLength(final int length) {
+      final java.util.List<Term> javaList = new ArrayList<Term>();
+      for (int i = 0; i < length; i++) {
+         javaList.add(new Variable("E" + i));
+      }
+      return createList(javaList);
+   }
+
+   private boolean checkLength(final Term list, final Term expectedLength) {
       final java.util.List<Term> javaList = toJavaUtilList(list);
       if (javaList != null) {
          final IntegerNumber actualLength = new IntegerNumber(javaList.size());

32332b71225340471bd913241b24df37be493d18
s-webber <webbers.github@gmail.com>
2015-08-30 11:37:05 +0100

    corrected console startup shell scripts
    Use $ to indicate arguments (rather than windows style %) and use -p
    option of mkdir so that no warning is outputted if the directory already
    exists.

--- etc/projog-console.sh
+++ etc/projog-console.sh
@@ -1,2 +1,2 @@
-mkdir projogGeneratedClasses
-java -Dprojog.compile=true -Dprojog.generatedClasses=projogGeneratedClasses -cp projogGeneratedClasses:lib/projog-core.jar org.projog.tools.ProjogConsole %1 %2 %3 %4 %5 %6 %7 %8 %9
+mkdir -p projogGeneratedClasses
+java -Dprojog.compile=true -Dprojog.generatedClasses=projogGeneratedClasses -cp projogGeneratedClasses:lib/projog-core.jar org.projog.tools.ProjogConsole $1 $2 $3 $4 $5 $6 $7 $8 $9

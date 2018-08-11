f25bbd67174276fb16ef1817990238da77c10d58
s-webber <s-webber@users.noreply.github.com>
2018-08-11 20:47:11 +0100

    made AbstractCalculatable public
    Made AbstractCalculatable (and its abstract subclasses) public so they
    can be extended by other projects which use Projog.

--- src/core/org/projog/core/function/math/AbstractTwoArgumentsCalculatable.java
+++ src/core/org/projog/core/function/math/AbstractTwoArgumentsCalculatable.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013-2014 S. Webber
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,7 +23,7 @@ import org.projog.core.term.TermType;
 /**
  * A template for {@code Calculatable}s that accept two arguments.
  */
-abstract class AbstractTwoArgumentsCalculatable extends AbstractCalculatable {
+public abstract class AbstractTwoArgumentsCalculatable extends AbstractCalculatable {
    @Override
    public final Numeric calculate(Numeric n1, Numeric n2) {
       if (containsFraction(n1, n2)) {

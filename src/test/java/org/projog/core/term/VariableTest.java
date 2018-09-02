eb1749f3e7953d67e5f592cda9480c8e9e172517
s-webber <s-webber@users.noreply.github.com>
2018-09-02 11:04:52 +0100

    test of infinite terms - #124
    Added unit-test to confirm that a StackOverflowError is thrown when
    using infinite terms (also known as "cyclic terms").

--- src/test/java/org/projog/core/term/VariableTest.java
+++ src/test/java/org/projog/core/term/VariableTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013-2014 S. Webber
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -219,5 +219,33 @@ public class VariableTest {
 
       v1.backtrack();
       assertSame(v1, v1.getTerm());
+   }
+
+   @Test
+   public void testInfiniteTerm() {
+      Variable v = variable("X");
+      Structure t = structure("name", v);
+      assertTrue(v.unify(t));
+
+      try {
+         v.getTerm();
+         fail();
+      } catch (StackOverflowError e) {
+      }
+      try {
+         t.getTerm();
+         fail();
+      } catch (StackOverflowError e) {
+      }
+      try {
+         v.toString();
+         fail();
+      } catch (StackOverflowError e) {
+      }
+      try {
+         t.toString();
+         fail();
+      } catch (StackOverflowError e) {
+      }
    }
 }

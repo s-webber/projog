172c1585ec5f010962480fb8eca07b33b0e4701c
s-webber <s-webber@users.noreply.github.com>
2018-10-28 15:31:19 +0000

    functor/3 supports creation of atoms - resolves #160

--- src/main/java/org/projog/core/function/construct/Functor.java
+++ src/main/java/org/projog/core/function/construct/Functor.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2013-2014 S. Webber
- * 
+ * Copyright 2013 S. Webber
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,6 @@
 package org.projog.core.function.construct;
 
 import static org.projog.core.term.TermUtils.createAnonymousVariable;
-import static org.projog.core.term.TermUtils.getAtomName;
 import static org.projog.core.term.TermUtils.toInt;
 
 import org.projog.core.ProjogException;
@@ -25,6 +24,7 @@ import org.projog.core.term.Atom;
 import org.projog.core.term.IntegerNumber;
 import org.projog.core.term.Structure;
 import org.projog.core.term.Term;
+import org.projog.core.term.TermType;
 
 /* TEST
  %QUERY functor(f(a,b,c(Z)),F,N)
@@ -52,11 +52,30 @@ import org.projog.core.term.Term;
  % N=0
  %ANSWER
 
+ %QUERY functor(X,x,0)
+ %ANSWER X=x
+
+ %QUERY functor(X,x,1)
+ %ANSWER X=x(_)
+
+ %QUERY functor(X,x,2)
+ %ANSWER X=x(_, _)
+
+ %QUERY functor(X,x,3)
+ %ANSWER X=x(_, _, _)
+
+ %TRUE functor(x,x,0)
+ %FALSE functor(x,x,3)
+
+ %TRUE functor(x(1,2,3),x,3)
+ %FALSE functor(x(1,2,3),y,3)
+ %FALSE functor(x(1,2,3),x,0)
+ %FALSE functor(x(1,2,3),x,1)
+ %FALSE functor(x(1,2,3),x,2)
+ %FALSE functor(x(1,2,3),x,4)
+
  %FALSE functor([a,b,c],'.',3)
  %FALSE functor([a,b,c],a,Z)
-
- %QUERY functor( X, sentence, 2)
- %ANSWER X = sentence(_, _)
 
  copy(Old, New) :- functor(Old, F, N), functor(New, F, N).
 
@@ -72,24 +91,43 @@ import org.projog.core.term.Term;
  */
 public final class Functor extends AbstractSingletonPredicate {
    @Override
-   public boolean evaluate(Term t, Term f, Term n) {
-      switch (t.getType()) {
+   public boolean evaluate(Term term, Term functor, Term arity) {
+      switch (term.getType()) {
          case ATOM:
-            return f.unify(t) && n.unify(new IntegerNumber(0));
+            return functor.unify(term) && arity.unify(new IntegerNumber(0));
          case STRUCTURE:
          case LIST:
          case EMPTY_LIST:
-            return f.unify(new Atom(t.getName())) && n.unify(new IntegerNumber(t.getNumberOfArguments()));
+            return functor.unify(new Atom(term.getName())) && arity.unify(new IntegerNumber(term.getNumberOfArguments()));
          case NAMED_VARIABLE:
-            int numArgs = toInt(n);
-            Term[] a = new Term[numArgs];
-            for (int i = 0; i < numArgs; i++) {
-               a[i] = createAnonymousVariable();
-            }
-            String functorName = getAtomName(f);
-            return t.unify(Structure.createStructure(functorName, a));
+            Term createdTerm = createTerm(functor, arity);
+            return term.unify(createdTerm);
          default:
-            throw new ProjogException("Invalid type for first argument of Functor command: " + t.getType());
+            throw new ProjogException("Invalid type for first argument of Functor command: " + term.getType());
+      }
+   }
+
+   /**
+    * Creates a term using the given functor (name) and arity (number of arguments).
+    *
+    * @param functor an atom representing the name of the term to create
+    * @param arity a numeric representing the number of arguments of the term to create
+    * @return if arity is 0 then an atom will be returned, else a structure will be created.
+    */
+   private Term createTerm(Term functor, Term arity) {
+      if (functor.getType() != TermType.ATOM) {
+         throw new ProjogException("Expected atom but got: " + functor.getType() + " " + functor);
+      }
+
+      int numArgs = toInt(arity);
+      if (numArgs == 0) {
+         return functor;
+      } else {
+         Term[] args = new Term[numArgs];
+         for (int i = 0; i < numArgs; i++) {
+            args[i] = createAnonymousVariable();
+         }
+         return Structure.createStructure(functor.getName(), args);
       }
    }
 }

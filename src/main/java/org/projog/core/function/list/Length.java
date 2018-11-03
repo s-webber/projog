89a6ce96c691a4b6ea10148fd3e9b3c9afa72532
s-webber <s-webber@users.noreply.github.com>
2018-11-03 13:04:27 +0000

    length/2 supports both arguments being variables - resolves #162

--- src/main/java/org/projog/core/function/list/Length.java
+++ src/main/java/org/projog/core/function/list/Length.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2013-2014 S. Webber
- * 
+ * Copyright 2013 S. Webber
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,10 +15,12 @@
  */
 package org.projog.core.function.list;
 
+import static org.projog.core.function.AbstractSingletonPredicate.toPredicate;
 import static org.projog.core.term.ListFactory.createListOfLength;
 import static org.projog.core.term.ListUtils.toJavaUtilList;
 
-import org.projog.core.function.AbstractSingletonPredicate;
+import org.projog.core.Predicate;
+import org.projog.core.function.AbstractPredicateFactory;
 import org.projog.core.term.IntegerNumber;
 import org.projog.core.term.Term;
 import org.projog.core.term.TermUtils;
@@ -32,20 +34,55 @@ import org.projog.core.term.TermUtils;
  %ANSWER X=2
  %QUERY length([a,b,c],X)
  %ANSWER X=3
- 
+
  %FALSE length([a,b|c],X)
  %FALSE length([a,b],1)
  %FALSE length([a,b],3)
  %FALSE length(abc,3)
- 
+
  %QUERY length(X,0)
  %ANSWER X=[]
- 
+
  %QUERY length(X,1)
  %ANSWER X=[E0]
- 
+
  %QUERY length(X,3)
  %ANSWER X=[E0,E1,E2]
+
+ %QUERY length(X,Y)
+ %ANSWER
+ % X=[]
+ % Y=0
+ %ANSWER
+ %ANSWER
+ % X=[E0]
+ % Y=1
+ %ANSWER
+ %ANSWER
+ % X=[E0,E1]
+ % Y=2
+ %ANSWER
+ %ANSWER
+ % X=[E0,E1,E2]
+ % Y=3
+ %ANSWER
+ %ANSWER
+ % X=[E0,E1,E2,E3]
+ % Y=4
+ %ANSWER
+ %ANSWER
+ % X=[E0,E1,E2,E3,E4]
+ % Y=5
+ %ANSWER
+ %ANSWER
+ % X=[E0,E1,E2,E3,E4,E5]
+ % Y=6
+ %ANSWER
+ %ANSWER
+ % X=[E0,E1,E2,E3,E4,E5,E6]
+ % Y=7
+ %ANSWER
+ %QUIT
  */
 /**
  * <code>length(X,Y)</code> - determines the length of a list.
@@ -54,14 +91,19 @@ import org.projog.core.term.TermUtils;
  * value <code>Y</code>.
  * </p>
  */
-public final class Length extends AbstractSingletonPredicate {
+public final class Length extends AbstractPredicateFactory {
    @Override
-   public boolean evaluate(final Term list, final Term expectedLength) {
-      if (list.getType().isVariable()) {
+   public Predicate getPredicate(final Term list, final Term expectedLength) {
+      boolean firstArgIsVariable = list.getType().isVariable();
+      boolean secondArgIsVariable = expectedLength.getType().isVariable();
+
+      if (firstArgIsVariable && secondArgIsVariable) {
+         return new Retryable(list, expectedLength);
+      } else if (firstArgIsVariable) {
          final int length = TermUtils.toInt(expectedLength);
-         return list.unify(createListOfLength(length));
+         return toPredicate(list.unify(createListOfLength(length)));
       } else {
-         return checkLength(list, expectedLength);
+         return toPredicate(checkLength(list, expectedLength));
       }
    }
 
@@ -72,6 +114,32 @@ public final class Length extends AbstractSingletonPredicate {
          return expectedLength.unify(actualLength);
       } else {
          return false;
+      }
+   }
+
+   private static class Retryable implements Predicate {
+      int i = 0;
+      final Term list;
+      final Term length;
+
+      private Retryable(Term list, Term length) {
+         this.list = list;
+         this.length = length;
+      }
+
+      @Override
+      public boolean evaluate() {
+         list.backtrack();
+         length.backtrack();
+         list.unify(createListOfLength(i));
+         length.unify(new IntegerNumber(i));
+         i++;
+         return true;
+      }
+
+      @Override
+      public boolean couldReevaluationSucceed() {
+         return true;
       }
    }
 }

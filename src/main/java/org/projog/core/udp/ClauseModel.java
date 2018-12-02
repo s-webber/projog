99e41444c0d79dbe53177b81c765eb993e007ba4
s-webber <s-webber@users.noreply.github.com>
2018-12-02 19:18:30 +0000

    Readded getAntecedent to ClauseModel
    Had renamed getAntecedent to getAntecedant (as was spelt incorrectly)
    but have now decided to readd getAntecedent as is still referenced by
    the projog-examples module. Have marked getAntecedent as deprecated.

--- src/main/java/org/projog/core/udp/ClauseModel.java
+++ src/main/java/org/projog/core/udp/ClauseModel.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2013-2014 S. Webber
- * 
+ * Copyright 2013 S. Webber
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -68,10 +68,24 @@ public final class ClauseModel {
       this.antecedent = antecedent;
    }
 
+   /**
+    * Replaced by {{@link #getAntecedent()} but marking as deprecated, rather than removing, as is used in
+    * projog-examples.
+    *
+    * @deprecated
+    * @see #getAntecedent
+    */
+   @Deprecated
+   public Term getAntecedant() {
+      return getAntecedent();
+   }
+
+   /** Returns the body of the clause. i.e. the bit after the {@code :-} */
    public Term getAntecedent() {
       return antecedent;
    }
 
+   /** Returns the head of the clause. i.e. the bit before the {@code :-} */
    public Term getConsequent() {
       return consequent;
    }
